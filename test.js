163b300ff8d62ee8a371b7a879b807263a41d81a
Eric Crosson <eric.s.crosson@utexas.edu>
2022-11-05 11:21:51 -0500

    test: replace test with built-in test runner

--- test.js
+++ test.js
@@ -1,19 +1,15 @@
-const fs = require('fs');
+const fs = require('node:fs');
+const test = require('node:test');
+
 const eslint = require('eslint');
 
 function getErrors(configFile, fileToTest) {
-
   const cli = new eslint.CLIEngine({
     configFile: configFile,
   });
-
   return cli.executeOnText(fs.readFileSync(fileToTest, 'utf8'));
 }
 
-describe('Validate ESLint configs', () => {
-  ['test.js'].forEach((file) => {
-    it(`load config ${file} in ESLint to validate all rules are correct`, () => {
-      getErrors('index.js', file)
-    });
-  });
-});
+test('should run eslint over test.js to validate all rules are correct', () => {
+  getErrors('index.js', 'test.js');
+})
